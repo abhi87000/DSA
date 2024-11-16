3426d9baea9e646bf8492bc6ef70807de05a9686
abhi87000 <gautamabhishek87000@gmail.com>
2024-11-16 14:24:00 +0530

    changes in lru

--- src/Main.java
+++ src/Main.java
@@ -5,86 +5,23 @@
 
 public class Main {
 
-    public class LRU{
-        Node head =new Node(-1,-1);
-        Node tail=new Node(-1,-1);
-        int capacity;
-
-        HashMap<Integer,Node> hashMap=new HashMap<>();
-
-        public LRU(int _capacity) {
-            capacity = _capacity;
-            head.next = tail;
-            tail.prev = head;
-        }
-        void add(Integer key,Integer val)
-        {
-            if(hashMap.containsKey(key))
-            {
-                deletenode(hashMap.get(key));
-            }
-            if(hashMap.size()==100)
-            {
-                Node toDelete=tail.prev;
-                deletenode(toDelete);
-            }
-            addNode(new Node(key,val));
-        }
-
-        private void deletenode(Node node) {
-            hashMap.remove(node.key);
-            Node next=node.next;
-            Node prev=node.prev;
-
-            prev.next=next;
-            next.prev=prev;
-        }
-
-        private void addNode(Node node) {
-            hashMap.put(node.key,node);
-                Node temp=head.next;
-                node.next=temp;
-                head.next=node;
-                node.prev=head;
-                temp.prev=node;
-        }
-
-        void delete(Integer key)
-        {
-            Node toDelete=hashMap.get(key);
-            deletenode(toDelete);
-        }
-
-        Integer get(Integer key)
-        {
-            if(hashMap.containsKey(key))
-            {
-
-                Node temp=hashMap.get(key);
-                deletenode(temp);
-                addNode(temp);
-                return temp.val;
-
-            }
-            return -1;
-        }
-        class Node{
-            int key;
-            int val;
-            Node next;
-            Node prev;
-
-            Node(int k,int val)
-            {
-                this.key=k;
-                this.val=val;
-            }
-        }
-    }
     public static void main(String[] args) {
+        LRU lru=new LRU(2);
+        lru.add(1,2);
+        lru.add(2,4);
+        lru.add(1,3);
+        lru.add(3,3);
+        lru.add(4,6);
+
+//        lru.hashMap.entrySet().stream().forEach((hash)-> System.out.println(hash.getKey()+" "+lru.get(hash.getKey())));
+        new ArrayList<>(lru.hashMap.keySet())
+                .forEach(key -> System.out.println(key + " " + lru.get(key)));
+//        for(Map.Entry<Integer, LRU.Node> entry:lru.hashMap.entrySet())
 
 
-
+//        lru.hashMap.forEach((key,val)-> System.out.println(key +" "+ lru.get(key)));
+//        lru.hashMap.entrySet().stream().forEach(integerNodeEntry -> System.out.print
+//        ln(integerNodeEntry.getKey()+" "+integerNodeEntry.getValue().g));
 
     }
 }
